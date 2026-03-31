072b24230cd5f216391793468bc1c65c63cca12b
dn0218 <132123794+dn0218@users.noreply.github.com>
2026-03-31 12:46:50 +0800

    Update selinux_hardening.sh

--- scripts/selinux_hardening.sh
+++ scripts/selinux_hardening.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+echo "== SELinux  =="
+
+setsebool -P httpd_can_connect_zabbix on
+setsebool -P httpd_can_network_connect_db on
+
+echo "== 验证上下文 =="
+ls -dZ /etc/zabbix/
+
+echo "== Current SELinux State =="
+getenforce
+
+echo "✅ SELinux enhanced（Keep Enforcing）"
 
